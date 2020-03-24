78ffb9ef20bea91f4a270538bbd6db8fb5555d2e
Martin <martinmorlot@gmail.com>
2020-03-24 15:49:21 +0100

    location script in python done

---
+++ location.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def location():
+    api_url = "https://demo.ip-api.com/json/?fields=33288191&lang=en"
+    headers = {
+        "Host": "demo.ip-api.com",
+        "Origin": "https://ip-api.com",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-Fetch-Mode": "cors",
+        "Referer": "https://ip-api.com/"
+    }
+    result = requests.get(api_url, headers=headers).json()
+    lat, lon = result['lat'], result['lon']
+    return lat, lon
+
+
+if __name__ == "__main__":
+    print(location())
